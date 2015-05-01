3f8566eaa91e2ba2ab59f68c28538a67cefeaf69
Adam Proctor <adam@uken.com>
2015-05-01 18:54:17 -0400

    basic button stuff

---
+++ Assets/Scripts/Props/ButtonProp.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class ButtonProp : MonoBehaviour {
+
+	public enum ButtonType {
+		Hold,
+		Switch,
+		Toggle
+	}
+
+	public enum ButtonState {
+		Idle,
+		Pushed,
+		Locked
+	}
+
+	public ButtonType buttonType = ButtonType.Hold;
+
+	public ButtonState state = ButtonState.Idle;
+
+	public GameObject target = null;
+	public UnityEvent buttonPushed;
+	public UnityEvent buttonReleased;
+
+	void OnTriggerEnter(Collider other) {
+		if (state == ButtonState.Locked) {
+			return;
+		}
+		if (other.GetComponent<Blob> ()) {
+			//TODO abstract this so others can flick switches
+			state = ButtonState.Pushed;
+			buttonPushed.Invoke();
+			if(buttonType == ButtonType.Switch) {
+				state = ButtonState.Locked;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.GetComponent<Blob> ()) {
+			//TODO check for multiple things pressing the button
+			buttonReleased.Invoke();
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Props/Door.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public void OpenDoor() {
+		Debug.LogError ("OPENING");
+	}
+}
