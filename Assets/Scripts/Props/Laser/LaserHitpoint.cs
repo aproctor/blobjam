4cfc39dd6dbddd436ed1bdb6074d0ee9071f557c
Adam Proctor <adam@uken.com>
2015-05-02 18:23:22 -0400

    firing my laser

---
+++ Assets/Scripts/Props/Laser/LaserHitpoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserHitpoint : MonoBehaviour {
+
+	private Vector3 originalPosition = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		this.originalPosition = this.transform.position;
+	}
+
+	public void Reset () {
+		this.transform.position = this.originalPosition;
+	}
+
+}
