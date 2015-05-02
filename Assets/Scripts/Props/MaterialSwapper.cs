ca2e03f5966d9d4d418c926512bffbe184fd3a8c
Adam Proctor <adam@uken.com>
2015-05-02 00:04:35 -0400

    swap materials on collision with mat swapper prop

---
+++ Assets/Scripts/Props/MaterialSwapper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialSwapper : MonoBehaviour {
+
+
+	public BlobMaterial mat;
+
+	void OnTriggerEnter(Collider other) {
+		Blob blob = other.GetComponent<Blob> ();
+		if (blob) {
+			blob.ApplyMat(mat);
+		}
+	}
+}
