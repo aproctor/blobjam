18ad78ecd862312f3262b140a04b656929206b89
Adam Proctor <adam@uken.com>
2015-05-03 14:53:08 -0400

    blob multi selection

--- Assets/Scripts/Core/BlobGame.cs
+++ Assets/Scripts/Core/BlobGame.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BlobGame : MonoBehaviour {
 	
 	public GameHud hud;
 
-	public int NumBlobs { get; private set; }
+	private List<Blob> blobs = new List<Blob> ();
+	public int NumBlobs {
+		get {
+			return this.blobs.Count;
+		}
+	}
 
 	private static BlobGame _instance = null;
 	public static BlobGame Instance {
@@ -28,6 +34,45 @@ public class BlobGame : MonoBehaviour {
 		Over
 	}
 	public GameState state = GameState.Loading;
+	
+	public void ToggleSelection() {
+		List<Blob> selectedBlobs = new List<Blob>();
+		int curSelected = 0;
+		int i = 0;
+		foreach (Blob b in this.blobs) {
+			if(b.Selected) {
+				selectedBlobs.Add(b);
+				curSelected = i;
+			}
+			i++;
+		}
+
+		//Deselect all blobs
+		foreach (Blob b in selectedBlobs) {
+			b.Selected = false;
+		}
+
+		if (selectedBlobs.Count > 1) {
+			//Select the first blob in the list (others were just deselected)
+			selectedBlobs [0].Selected = true;
+		} else if(selectedBlobs.Count == 1){
+			//Select the next blob based on curSelected
+			int targetIndex = ++curSelected % this.blobs.Count;
+			bool SELECT_ALL_ON_LAST = true;
+
+			if(targetIndex == 0 && SELECT_ALL_ON_LAST) {
+				foreach (Blob b in this.blobs) {
+					b.Selected = true;
+				}
+			} else {
+				this.blobs[targetIndex].Selected = true;
+			}
+
+		} else if(this.blobs.Count > 0) {
+			//Select the first blob if none were selected for some reason
+			this.blobs[0].Selected = true;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -36,7 +81,6 @@ public class BlobGame : MonoBehaviour {
 			DontDestroyOnLoad(this);
 
 			this.levelManager = this.GetComponent<LevelManager>();
-			this.NumBlobs = 1;
 
 			string[] layers = new string[] {
 				"slime","rock", "ice", "fire", "water"
@@ -54,12 +98,22 @@ public class BlobGame : MonoBehaviour {
 
 	void Update() {
 		if (this.state == GameState.Over) {			
-			if(Input.GetButtonDown("Cancel")) {
-				this.levelManager.ReloadLevel();
+			if (Input.GetButtonDown ("Cancel")) {
+				this.levelManager.ReloadLevel ();
 			}
-			if(Input.GetButtonDown("Jump")) {
-				this.levelManager.LoadMenu();
+			if (Input.GetButtonDown ("Jump")) {
+				this.levelManager.LoadMenu ();
 			}
+		} else {
+			//TODO only do this if playing, but state machine isn't fully rigged upf or development scenes
+			this.UpdatePlaying();
+		}
+
+	}
+
+	void UpdatePlaying() {
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			this.ToggleSelection();
 		}
 	}
 
@@ -67,15 +121,15 @@ public class BlobGame : MonoBehaviour {
 		this.state = GameState.Playing;
 	
 		Blob[] blobs = GameObject.FindObjectsOfType<Blob> ();
-		this.NumBlobs = blobs.Length;
+		this.blobs.AddRange (blobs);
 	}
 
-	public void AddBlob() {
-		this.NumBlobs += 1;
+	public void AddBlob(Blob blob) {
+		this.blobs.Add (blob);
 	}
 
-	public void RemoveBlob () {
-		this.NumBlobs -= 1;
+	public void RemoveBlob (Blob blob) {
+		this.blobs.Remove (blob);
 
 		if(this.NumBlobs <= 0) {
 			//Not ready to do this, level loads cause loops
